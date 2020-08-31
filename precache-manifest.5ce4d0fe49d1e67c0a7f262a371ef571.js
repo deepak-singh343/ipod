35a7d91b5ea9b2ecac3594c11b62b2e855310ce1
Deepak <deepak.negi343@gmail.com>
2020-08-31 20:08:00 +0530

    Updates

---
+++ precache-manifest.5ce4d0fe49d1e67c0a7f262a371ef571.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54f150f1d9b1d1344a2d26e30468d165",
+    "url": "/ipod/index.html"
+  },
+  {
+    "revision": "cc84f6d2a3e3b1bb5f46",
+    "url": "/ipod/static/css/main.262c31b1.chunk.css"
+  },
+  {
+    "revision": "c3df88b6c8a098558305",
+    "url": "/ipod/static/js/2.fff3537c.chunk.js"
+  },
+  {
+    "revision": "3b6069e01f0db714f9e787b4dbe9775c",
+    "url": "/ipod/static/js/2.fff3537c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc84f6d2a3e3b1bb5f46",
+    "url": "/ipod/static/js/main.dbadc11d.chunk.js"
+  },
+  {
+    "revision": "803599ccb544e101b912",
+    "url": "/ipod/static/js/runtime-main.ccdb9399.js"
+  }
+]);
